e2929677a84afa09bf31499c017e780329642da8
ELGhifari11 <ghifariakun@gmail.com>
2025-07-07 20:23:52 +0700

    feat(UserResource): add password generation and WhatsApp notification
    - Generate random password with mixed characters
    - Send password via WhatsApp instead of email
    - Add success notification with creation details

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
+use App\Filament\Resources\UserResource;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use App\Http\Controllers\WhatsAppController;
 
 class CreateUser extends CreateRecord
 {
@@ -12,8 +15,41 @@ class CreateUser extends CreateRecord
     protected static bool $canCreateAnother = false;
 
     //customize redirect after create
-    public function getRedirectUrl(): string
+      public function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+
+        $password = Str::random(8) . Str::random(4, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') . Str::random(2, '!@#$%^&*()') . Str::random(2, '0123456789');
+
+        $data['role'] = 'mentor';
+
+        $data['password'] = bcrypt($password);
+
+        // Send email with login credentials
+        // Mail::send('emails.user-credentials', [
+        //     'email' => $data['email'],
+        //     'password' => $password,
+        // ], function($message) use ($data) {
+        //     $message->to($data['email'])
+        //             ->subject('Your Login Credentials');
+        // });
+
+        $whatsAppController = new WhatsAppController();
+        $whatsAppController->messagePasswordRegister($data['phone'], $password, $data['name'], now()->toDateString() . ' ' . now()->format('l'));
+
+        return $data;
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->icon('heroicon-s-user-plus')
+            ->title('User registered')
+            ->body('The user has been created successfully, The password has been sent to the user WhatsApp.');
     }
 }
