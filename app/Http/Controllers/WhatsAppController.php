374134e64924176240bbb724015fedd38c279bae
ELGhifari11 <ghifariakun@gmail.com>
2025-07-07 20:24:04 +0700

    feat(WhatsApp): add controller for sending WhatsApp messages
    Implement WhatsAppController with methods to send generic messages and registration confirmation messages via Fonnte API

---
+++ app/Http/Controllers/WhatsAppController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WhatsAppController extends Controller
+{
+    public function sendMessage($phone, $message)
+    {
+        $phone = preg_replace('/\D/', '', $phone);
+
+        $url = 'https://api.fonnte.com/send';
+        $token = '';
+        $data = [
+            'target' => $phone,
+            'message' => $message,
+            'countryCode' => '62'
+        ];
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: ' . $token
+        ]);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        logger()->info($response);
+    }
+
+    public function messagePasswordRegister($phone, $password, $name, $currentDate)
+    {
+        $message = "*📋 REGISTRASI AKUN BERHASIL* :\n\n" .
+            "Date: {$currentDate}\n\n" .
+            "Nama: *{$name}*\n\n" .
+            "Password: `{$password}`\n\n" .
+            "Penting: Login dengan password diatas dan segera ubah.\n\n" .
+            "\n\nLink website: " . env('APP_URL') . "/login \n\n" .
+            "*Jika Anda membutuhkan bantuan, hubungi kami.*";
+
+        $this->sendMessage($phone, $message);
+        return $message;
+    }
+}
