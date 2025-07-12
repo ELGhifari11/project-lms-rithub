ff1ae152ac9b15c51728a1350553d939a84d8bd1
ELGhifari11 <ghifariakun@gmail.com>
2025-07-12 22:36:35 +0700

    feat(routes): add verified email endpoint for registration

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\EmailVerificationController;
+use App\Http\Controllers\RegisterController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
+Route::get('/v1/verified-email', [RegisterController::class, 'verifiedEmail']);
